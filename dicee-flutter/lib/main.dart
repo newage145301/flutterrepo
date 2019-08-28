b0d07e05c34d7539564c03c60c6e9e4e13a8dd01
newage145301 <54566693+newage145301@users.noreply.github.com>
2019-08-28 20:43:37 +0300

    Update main.dart

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,20 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dice1Value = 1;
+  int dice2Value = 1;
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'), onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  dice1Value=Random().nextInt(7);
+                  if(dice1Value<=0)dice1Value=1;
+
+                  dice2Value=Random().nextInt(7);
+                  if(dice2Value<=0)dice2Value=1;
+
+                });
+              },
+              child: Image.asset('images/dice$dice1Value.png'),
             ),
           ),
           Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice2.png'), onPressed: () {},
+            child:FlatButton(
+              onPressed: () {
+                setState(() {
+                  dice1Value=Random().nextInt(7);
+                  if(dice1Value<=0)dice1Value=1;
+
+                  dice2Value=Random().nextInt(7);
+                  if(dice2Value<=0)dice2Value=1;
+                });
+              },
+              child: Image.asset('images/dice$dice2Value.png'),
             ),
           ),
         ],
