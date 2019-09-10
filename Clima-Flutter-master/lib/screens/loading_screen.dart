f982835612786cca908afa65ef6cdd5e083b7402
newage145301 <54566693+newage145301@users.noreply.github.com>
2019-09-10 16:20:10 +0300

    clima

--- Clima-Flutter-master/lib/screens/loading_screen.dart
+++ Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,10 +11,33 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    var location =Location();
-    await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latidute);
+//    var location = Location();
+//    await location.getCurrentLocation();
+//    print(location.longitude);
+//    print(location.latidute);
+
+    getDataFromApi();
+  }
+
+  void getDataFromApi() async {
+    http.Response response;
+
+    try
+    {
+      response = await http.get(
+          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    String data =response.body;
+      int id= jsonDecode(data)['weather'][0]['id'];
+      String cityName= jsonDecode(data)['name'];
+      double temp= jsonDecode(data)['main']['temp'];
+
+      print('ID:$id CityName:$cityName Temprature:$temp');
+    } catch(e)
+    {
+
+    }
+
+
   }
 
   @override
