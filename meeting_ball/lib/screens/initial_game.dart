c17f22ef01ef645156f771790784ac8f8e7ff95a
BULENTHOME\Bulent <bulentgokdas@gmail.com>
2019-10-03 08:44:31 +0300

    meeting

---
+++ meeting_ball/lib/screens/initial_game.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InitialGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body:Text('kdsldfkjsldkfjsldkjflsd')
+    );
+  }
+}
+
+
