0364fabe6face710e0a01e53015f4defbac5a49e
BULENTHOME\Bulent <bulentgokdas@gmail.com>
2019-10-15 07:22:59 +0300

    back again

--- meeting_ball3/lib/screens/meeting_room.dart
+++ meeting_ball3/lib/screens/meeting_room.dart
@@ -98,13 +98,9 @@ class MeetingRoom extends StatelessWidget {
                  return Column(
                    children: textList,
                  );
-<<<<<<< HEAD
                } else return Column(children: <Widget>[
                  Text('Not user yet')]);
 
-=======
-               }
->>>>>>> parent of 6233897... jgjhgjh
            },
          )
 
